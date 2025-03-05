55d944dd6166bab695ea31758023d96c719f23b1
Repositorio Cursos <152635234+repocurso@users.noreply.github.com>
2025-03-05 20:26:23 +0100

    Creación Repositorio

--- integra2/SimpleHandler.py
+++ integra2/SimpleHandler.py
@@ -6,7 +6,7 @@ class SimpleHandler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header("Content-type", "text/html; charset=utf-8")
         self.end_headers()
-        self.wfile.write("¡Hola, CI/CD con Ansible y Python! {hostname}".encode('utf-8'))
+        self.wfile.write("CI/CD con Ansible y Python {hostname}".encode('utf-8'))
 
 server = HTTPServer(("0.0.0.0", 8000), SimpleHandler)
 server.serve_forever()
